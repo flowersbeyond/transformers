b38ac362254742f3177c1dd833426f19987e4b3b
Xinrui Guo <xingu@microsoft.com>
2020-01-03 14:53:40 +0800

    temporarily disable multi-gpu evaluate since this could cause error.

--- examples/run_multiple_choice.py
+++ examples/run_multiple_choice.py
@@ -260,8 +260,9 @@ def evaluate(args, model, tokenizer, prefix="", test=False):
         eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=args.eval_batch_size)
 
         # multi-gpu evaluate
-        if args.n_gpu > 1:
-            model = torch.nn.DataParallel(model)
+        # TODO: temporarily disable multi-gpu evaluate since this could cause error.
+        #if args.n_gpu > 1:
+        #    model = torch.nn.DataParallel(model)
 
         # Eval!
         logger.info("***** Running evaluation {} *****".format(prefix))
